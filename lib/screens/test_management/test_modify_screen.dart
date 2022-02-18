e56712958c9a03fc26bf9ccfa6dd87ea8eaab999
Arenslien <arenslien@gmail.com>
2022-02-18 21:12:37 +0900

    merge 작업 2\

--- lib/screens/test_management/test_modify_screen.dart
+++ lib/screens/test_management/test_modify_screen.dart
@@ -258,7 +258,7 @@ class _TestInputScreenState extends State<TestModifyScreen> {
                                                     items: selectedProgramField == null
                                                         ? null
                                                         : context.read<DBNotifier>().readSubFieldList(selectedProgramField!).map((value) {
-                                                            return DropdownMenuItem(value: value.subFieldName, child: Text(value.subFieldName));
+                                                            return DropdownMenuItem(value: value.title, child: Text(value.title));
                                                           }).toList(),
                                                     onChanged: (String? value) {
                                                       setState1(() {
