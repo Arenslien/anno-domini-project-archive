3fb79c4d298c43339e8b48965521eb166b998dc7
Arenslien <arenslien@gmail.com>
2022-07-26 22:11:28 +0900

    Docs: 프로젝트 파일 디렉토리 수정
    프로젝트 파일 디렉토리 수정 완료
    
    해결

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bibler',
       theme: ThemeData(
         // This is the theme of your application.
         //
