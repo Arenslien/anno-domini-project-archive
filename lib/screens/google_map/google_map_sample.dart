56ab8dc42c03e2b6fa726c0d8134d8615e14e33c
KimMS <softkimms@gmail.com>
2022-07-29 02:41:29 +0900

    feat: Get information of current location from GoogleMap

--- lib/screens/google_map/google_map_sample.dart
+++ lib/screens/google_map/google_map_sample.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapSample extends StatefulWidget {
@@ -11,24 +14,18 @@ class GoogleMapSample extends StatefulWidget {
 }
 
 class _GoogleMapSampleState extends State<GoogleMapSample> {
-  final Completer<GoogleMapController> _controller = Completer();
+  Completer<GoogleMapController> _controller = Completer();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  Set<Marker> markers = {};
 
-  static const CameraPosition _kLake = CameraPosition(
-    bearing: 192.8334901395799,
-    target: LatLng(37.43296265331129, -122.08832357078792),
-    tilt: 59.440717697143555,
-    zoom: 19.151926040649414,
-  );
+  static final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
+        markers: markers,
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
@@ -36,17 +33,58 @@ class _GoogleMapSampleState extends State<GoogleMapSample> {
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          _goToTheLake();
+        onPressed: () async {
+          Position position = await _goToTheCurrent();
+          final GoogleMapController controller = await _controller.future;
+          controller
+              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 14,
+          )));
+
+          late double latitude = position.latitude;
+          late double longitude = position.longitude;
+          print(position);
+          markers.clear();
+
+          markers.add(Marker(
+            markerId: MarkerId('current Location'),
+            position: LatLng(position.latitude, position.longitude),
+          ));
+
+          setState(() {});
         },
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+        label: Text('User current location'),
+        icon: Icon(Icons.location_on),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Future<Position> _goToTheCurrent() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location Permission denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
   }
 }
